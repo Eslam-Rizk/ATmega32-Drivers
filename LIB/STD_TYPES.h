c882fa3708a0257fa0a824a23b2dc80b0952ae5a
Eslam-Rizk <70345162+Eslam-Rizk@users.noreply.github.com>
2024-01-01 20:57:04 +0200

    Update STD_TYPES.h

--- LIB/STD_TYPES.h
+++ LIB/STD_TYPES.h
@@ -5,8 +5,6 @@ typedef unsigned char 		uint8;
 typedef signed char 		sint8;
 typedef unsigned short 		uint16;
 typedef signed short 		sint16;
-//typedef unsigned int 		uint16;
-//typedef signed int 		sint16;
 typedef unsigned int		uint32;
 typedef signed long 		sint32;
 typedef unsigned long long	uint64;
