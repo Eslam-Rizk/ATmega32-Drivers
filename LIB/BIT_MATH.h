feee505f14c694b8debfbffbbc678b8b9c1e0d17
Eslam-Rizk <70345162+Eslam-Rizk@users.noreply.github.com>
2024-03-30 13:56:51 +0200

    Delete LIB directory

--- LIB/BIT_MATH.h
+++
@@ -1,4 +0,0 @@
-#define GET_BIT(reg,bitNum)  	reg & (1<<bitNum)
-#define SET_BIT(reg,bitNum)  	reg|=(1<<bitNum)
-#define CLR_BIT(reg,bitNum)  	reg&=~(1<<bitNum)
-#define TOGGLE_BIT(reg,bitNum)  reg ^=(1<<bitNum)
