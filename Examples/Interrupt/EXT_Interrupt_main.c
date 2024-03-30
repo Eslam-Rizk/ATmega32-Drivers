4b065d631c2a343d8443f0b91075cc92197d211d
Eslam-Rizk <70345162+Eslam-Rizk@users.noreply.github.com>
2024-03-30 14:03:12 +0200

    Update EXT_Interrupt_main.c

--- Examples/Interrupt/EXT_Interrupt_main.c
+++ Examples/Interrupt/EXT_Interrupt_main.c
@@ -13,54 +13,59 @@
 #include "../MCAL/EXT_INT_v0/EXT_INT.h"
 #include "../MCAL/EXT_INT_v0/GIE.h"
 
-
+// Function prototypes for the ISR (Interrupt Service Routines)
 void EXTI0_ISR(void);
 void EXTI1_ISR(void);
 
 int main()
 {
-	//led pins as output
-	MDIO_vSetPinDir(DIO_PORTD , PIN0 , PIN_OUTPUT);
-	MDIO_vSetPinDir(DIO_PORTD , PIN5 , PIN_OUTPUT);
-	MDIO_vSetPinDir(DIO_PORTD , PIN6 , PIN_OUTPUT);
+    // Configure LED pins as output
+    MDIO_vSetPinDir(DIO_PORTD, PIN0, PIN_OUTPUT);
+    MDIO_vSetPinDir(DIO_PORTD, PIN5, PIN_OUTPUT);
+    MDIO_vSetPinDir(DIO_PORTD, PIN6, PIN_OUTPUT);
 
-	//external interrupt 0 , 1 pins
-	MDIO_vSetPinDir(DIO_PORTD , PIN2 , PIN_INPUT);
-	MDIO_vSetPinDir(DIO_PORTD , PIN3 , PIN_INPUT);
+    // Configure external interrupt pins
+    MDIO_vSetPinDir(DIO_PORTD, PIN2, PIN_INPUT);
+    MDIO_vSetPinDir(DIO_PORTD, PIN3, PIN_INPUT);
 
-	//ACTIVATE PULLUP on INT0,INT1
-	MDIO_vSetPinVal(DIO_PORTD , PIN2 , PIN_HIGH);
-	MDIO_vSetPinVal(DIO_PORTD , PIN3 , PIN_HIGH);
+    // Activate pull-up resistors on INT0 and INT1
+    MDIO_vSetPinVal(DIO_PORTD, PIN2, PIN_HIGH);
+    MDIO_vSetPinVal(DIO_PORTD, PIN3, PIN_HIGH);
 
-	//set call back functions
-	EXTI0_vSetCallBack(EXTI0_ISR);
-	EXTI1_vSetCallBack(EXTI1_ISR);
+    // Set callback functions for external interrupts
+    EXTI0_vSetCallBack(EXTI0_ISR);
+    EXTI1_vSetCallBack(EXTI1_ISR);
 
-	//enable interrupts
-	MEXTI_vEnINT(EXT_INT0);
-	MEXTI_vEnINT(EXT_INT1);
-	GIE_vEnable();
+    // Enable interrupts for INT0 and INT1
+    MEXTI_vEnINT(EXT_INT0);
+    MEXTI_vEnINT(EXT_INT1);
+    
+    // Enable global interrupts
+    GIE_vEnable();
 
-	while(1)
-	{
-		MDIO_vSetPinVal(DIO_PORTD , PIN0 , PIN_HIGH);
-		_delay_ms(1000);
-		MDIO_vSetPinVal(DIO_PORTD , PIN0 , PIN_LOW);
-		_delay_ms(1000);
-	}
-	return 0;
+    while(1)
+    {
+        // Toggle LED on PIN0 every 1 second
+        MDIO_vSetPinVal(DIO_PORTD, PIN0, PIN_HIGH);
+        _delay_ms(1000);
+        MDIO_vSetPinVal(DIO_PORTD, PIN0, PIN_LOW);
+        _delay_ms(1000);
+    }
+    return 0;
 }
 
-
+// External Interrupt 0 ISR
 void EXTI0_ISR(void)
 {
-	MDIO_vSetPinVal(DIO_PORTD , PIN5 , PIN_HIGH);
-	MDIO_vSetPinVal(DIO_PORTD , PIN6 , PIN_LOW);
+    // Set PIN5 high and PIN6 low
+    MDIO_vSetPinVal(DIO_PORTD, PIN5, PIN_HIGH);
+    MDIO_vSetPinVal(DIO_PORTD, PIN6, PIN_LOW);
 }
 
+// External Interrupt 1 ISR
 void EXTI1_ISR(void)
 {
-	MDIO_vSetPinVal(DIO_PORTD , PIN6 , PIN_HIGH);
-	MDIO_vSetPinVal(DIO_PORTD , PIN5 , PIN_LOW);
+    // Set PIN6 high and PIN5 low
+    MDIO_vSetPinVal(DIO_PORTD, PIN6, PIN_HIGH);
+    MDIO_vSetPinVal(DIO_PORTD, PIN5, PIN_LOW);
 }
-
